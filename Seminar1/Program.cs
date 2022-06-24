3470b42ee40cd7f678b9bb6d5161540bf2095b38
Vlad <vlad.melnik@bk.ru>
2022-06-25 00:28:17 +0300

    Task1 completed

---
+++ Seminar1/Program.cs
@@ -0,0 +1,11 @@
+﻿int num1, num2;
+Console.WriteLine("Input the first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+if(num1>num2)
+    Console.WriteLine("Max = " + num1);
+else if(num1==num2)
+Console.WriteLine("Numbers are equal");
+else
+    Console.WriteLine("Max = " + num2);
